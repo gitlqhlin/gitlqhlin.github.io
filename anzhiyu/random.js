4734dfa0b88a1d1dca7abd095b5fa9c5ddd4e452
gitlqhlin <lqhlin@gmail.com>
2025-10-03 21:53:01 +0800

    Site updated: 2025-10-03 21:53:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/03/hello-world/"];function toRandomPost(){
+var posts=["牧羊人/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
