84f1afb9fbf8f87e38cedf34e68dd0eb3acb2b0f
gitlqhlin <lqhlin@gmail.com>
2025-10-04 15:01:03 +0800

    Site updated: 2025-10-04 15:01:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["牧羊人/workers部署极简图床/","牧羊人/CF优选SaaS回源/","牧羊人/wokerAI部署文生图/","牧羊人/hello-world/"];function toRandomPost(){
+var posts=["牧羊人/workerAIflux1文生图/","牧羊人/tg图床/","牧羊人/workers部署极简图床/","牧羊人/CF优选SaaS回源/","牧羊人/wokerAI部署文生图/","牧羊人/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
